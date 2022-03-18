34851a01ac849373e2c52510a3ade66004a8d330
Isak Larsson <55209313+IsakLarsson@users.noreply.github.com>
2022-03-18 11:26:15 +0100

    restructuring of folders

--- backend/assets/game.js
+++ backend/assets/game.js
@@ -1,4 +1,6 @@
-const gameList = [{ id: "69", players: [{ name: "todd", move: "Scissors" }] }];
+const gameList = [
+    /* { id: "69", players: [{ name: "todd", move: "Scissors" }] } */
+];
 const allowedMoves = ["Rock", "Paper", "Scissors"];
 const winningMoves = { Rock: "Scissors", Paper: "Rock", Scissors: "Paper" };
 
